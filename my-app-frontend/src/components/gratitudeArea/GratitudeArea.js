f99495bd215608b169e11eb4064df880fb363ca4
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-08-09 11:11:14 -0400

    Components Implemented

---
+++ my-app-frontend/src/components/gratitudeArea/GratitudeArea.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GratitudeArea() {
+    return <h1>Gratitude area</h1>
+}
+
+export default GratitudeArea;
---
+++ my-app-frontend/src/components/newGratitudeEntry/NewGratitudeEntry.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewGratitudeEntry() {
+    return <h1>New entry</h1>
+}
+
+export default NewGratitudeEntry;
