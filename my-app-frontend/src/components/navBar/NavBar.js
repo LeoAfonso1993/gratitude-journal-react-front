de17393eb6a60656704b321c7c937d5d8e06e0d0
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-08-09 19:20:00 -0400

    New entry implemented

--- my-app-frontend/src/components/navBar/NavBar.js
+++ my-app-frontend/src/components/navBar/NavBar.js
@@ -1,7 +1,17 @@
-import React from "react"
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, Container} from 'react-bootstrap';
 
 function NavBar() {
-    return <h1>Nav</h1>
+    return (
+        <>
+        <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">BGrateful</Navbar.Brand>
+        </Container>
+      </Navbar>
+        </>
+    )
 }
 
 export default NavBar;
--- my-app-frontend/src/components/newGratitudeEntry/NewGratitudeEntry.js
+++ my-app-frontend/src/components/newGratitudeEntry/NewGratitudeEntry.js
@@ -1,7 +1,91 @@
-import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from "react";
+
 
 function NewGratitudeEntry() {
-    return <h1>New entry</h1>
+    const [categories, setCategories] = useState([])
+    const [blessings, setBlessings] = useState({
+        answer: "",
+        question_id: ""
+    })
+    const [catId, setCatID] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:9292/categories')
+        .then((response) => response.json())
+        .then((item) => setCategories(item))
+    },[])
+
+    const allTheOptions = categories.map((categ) => {
+        return (
+            <option>{categ.question}</option>
+        )
+    })
+
+    function handleChange(e) {
+        setBlessings({
+            [e.target.name]: e.target.value,
+        })
+        console.log(e.target.value)
+    }
+
+    function handleChangeCat(e){
+        for (let i of categories){
+            if (i.question === e.target.value){
+                setCatID(i.id)
+            }
+        }    
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        const blessingData = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                answer: blessings.answer,
+                question_id: catId
+            })
+        }
+
+        fetch('http://localhost:9292/home', blessingData)
+        .then((response) => response.json())
+        .then((item) => console.log(item))
+        .then(() => {
+            setBlessings({
+                answer: "",
+                question_id: ""
+            })
+        })
+        .then(() => window.alert('Blessing added successfully!'))
+    }
+
+
+  return (
+    <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="selectCategory">Select Category</Form.Label>
+          <Form.Select id="questionTextInput"
+          name="question_id"
+          onChange={handleChangeCat}
+          value={categories.question}>
+            {allTheOptions}
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control id="answerTextInput" 
+          placeholder="Answer"
+          name="answer"
+          onChange={handleChange}
+          value={blessings.answer} />
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+    </Form>
+  );
 }
 
 export default NewGratitudeEntry;
